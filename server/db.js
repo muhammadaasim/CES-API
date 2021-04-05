0e1e0936097fe9aecbd88047bdd817fe562795b1
tabarak5 <tacitim5@gmail.com>
2021-04-05 16:06:40 +0500

    Update db.js

--- server/db.js
+++ server/db.js
@@ -4,8 +4,8 @@ var mysql = require('mysql');
 // const db_connection = mysql.createPool({
 module.exports.connection = mysql.createPool({
 	host: 'localhost',
-	user: 'root',
-	password: 'mohsin11',
-	port: '3306',
+	user: 'mohsin',
+	password: 'mohsinsoomro',
+	port: '3306', 
 	database: 'ces'
 });
