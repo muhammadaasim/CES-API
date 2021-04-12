0335dc410c575a13fdf892cdff5fe33dfe9fafb6
tabarak5 <tacitim5@gmail.com>
2021-04-13 01:35:58 +0500

    added examsformwith subject
    Exam forms with subjects &
    specific student filter

--- server/routes/examform.js
+++ server/routes/examform.js
@@ -1,12 +1,13 @@
 var routes = require('express').Router();
 
-var {insertExamForm,UpdateStatus,getExamforms,getExamFormByStdId,insertSubjects,getExamformswithsubj} = require('../controller/examform')
+var {insertExamForm,UpdateStatus,getExamforms,getExamFormByStdId,insertSubjects,getExamformswithsubj,getExamFormwithsubByStdId} = require('../controller/examform')
 routes.get('/', getExamforms)
+routes.get('/subject', getExamformswithsubj)
+routes.get('/subject/:id', getExamFormwithsubByStdId)
 routes.get('/:id', getExamFormByStdId)
 routes.post('/', insertExamForm)
 routes.post('/subject/', insertSubjects)
 routes.put('/updatestatus/:formid',UpdateStatus);
-// routes.get('/subjects', getExamformswithsubj)
-// routes.get('/subjects/:id', getExamFormwithsubByStdId)
+
 
 module.exports = routes;
