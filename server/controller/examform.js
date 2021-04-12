4d38566def989ab71be9423037d8d41657d3fca1
tabarak5 <tacitim5@gmail.com>
2021-04-12 20:44:58 +0500

    form

--- server/controller/examform.js
+++ server/controller/examform.js
@@ -1,49 +1,49 @@
 var { connection } = require("../db");
-// module.exports.getStudent = (req, res) => {
-//   try {
-//     connection.getConnection((err, connection) => {
-//       if (err) throw err;
-//       console.log("MySQL Connection Established: ", connection.threadId);
-//       connection.query("SELECT * FROM GetStudent ", function (err, rows, fields) {
-//         if (err) throw err;
-//         console.log(rows);
-//         res.send({ error: "", success: "success", result: rows });
-//         connection.release((err) => console.log(err));
-//       });
-//     });
-//   } catch (e) {
-//     res.send({
-//       error: "Error getting student",
-//       result: [],
-//       success: "Failed",
-//     });
-//   }
-// };
-// module.exports.getStudentById = (req, res) => {
-//   try {
-//     connection.getConnection((err, connection) => {
-//       if (err) throw err;
-//       connection.query(
-//         "SELECT * FROM GetStudent WHERE student_id= ?",
-//         [req.params.id],
-//         function (err, rows, fields) {
-//           if (err) {
-//             console.log(err);
-//           }
-//           console.log(rows);
-//           res.send({ result: rows });
-//           connection.release((er) => console.log(er));
-//         }
-//       );
-//     });
-//   } catch (e) {
-//     res.send({
-//       error: "Error getting student",
-//       result: [],
-//       success: "Failed",
-//     });
-//   }
-// };
+module.exports.getExamforms = (req, res) => {
+  try {
+    connection.getConnection((err, connection) => {
+      if (err) throw err;
+      console.log("MySQL Connection Established: ", connection.threadId);
+      connection.query("SELECT * FROM examform ", function (err, rows, fields) {
+        if (err) throw err;
+        console.log(rows);
+        res.send({ error: "", success: "success", result: rows });
+        connection.release((err) => console.log(err));
+      });
+    });
+  } catch (e) {
+    res.send({
+      error: "Error getting examform",
+      result: [],
+      success: "Failed",
+    });
+  }
+};
+module.exports.getStudentById = (req, res) => {
+  try {
+    connection.getConnection((err, connection) => {
+      if (err) throw err;
+      connection.query(
+        "SELECT * FROM GetStudent WHERE student_id= ?",
+        [req.params.id],
+        function (err, rows, fields) {
+          if (err) {
+            console.log(err);
+          }
+          console.log(rows);
+          res.send({ result: rows });
+          connection.release((er) => console.log(er));
+        }
+      );
+    });
+  } catch (e) {
+    res.send({
+      error: "Error getting student",
+      result: [],
+      success: "Failed",
+    });
+  }
+};
 
 module.exports.insertExamForm = (req, res) => {
   try {
@@ -54,12 +54,12 @@ module.exports.insertExamForm = (req, res) => {
         [
           {
             std_id: req.body.std_id,
-            session:new Date().getFullYear(),
+            session: new Date().getFullYear(),
             sem_id: req.body.sem_id,
-            type:req.body.type,
-            status:0,
-            remarks:"",
-            date:new Date().getTime()
+            type: req.body.type,
+            status: 0,
+            remarks: "",
+            date: new Date().getTime(),
           },
         ],
         function (err, rows, fields) {
@@ -68,6 +68,35 @@ module.exports.insertExamForm = (req, res) => {
           res.send({
             error: null,
             message: "Insert successfully",
+            result: rows,
+          });
+          connection.release((er) => console.log(er));
+        }
+      );
+    });
+  } catch (e) {
+    res.send({
+      error: "Error getting student",
+      result: [],
+      success: "Failed",
+    });
+  }
+};
+
+module.exports.UpdateStatus = (req, res) => {
+  try {
+    connection.getConnection((err, connection) => {
+      if (err) throw err;
+      connection.query(
+        // `update config  SET isenabled = ?  WHERE id = ? `,[req.body.isenabled,req.params.id],
+        `UPDATE examform SET status = ? WHERE  id = ?`,
+        [req.body.status, req.params.formid],
+        function (err, rows, fields) {
+          if (err) throw err;
+          console.log(rows);
+          res.send({
+            error: null,
+            message: "Update successfully",
             result: rows,
           });
           connection.release((er) => console.log(er));
--- server/routes/examform.js
+++ server/routes/examform.js
@@ -1,8 +1,9 @@
 var routes = require('express').Router();
 
-var {insertExamForm} = require('../controller/examform')
-// routes.get('/', getDepartment)
+var {insertExamForm,UpdateStatus,getExamforms} = require('../controller/examform')
+routes.get('/', getExamforms)
 routes.post('/', insertExamForm)
+routes.put('/updatestatus/:formid',UpdateStatus);
 // routes.get('/:id', getDepartmentById)
 
 module.exports = routes;
