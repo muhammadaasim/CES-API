057dfdc74a08a0b7476cf961ea8c84336a553006
MOHSIN ALI SOOMRO <39213044+MohsinAliSoomro@users.noreply.github.com>
2021-04-13 14:05:11 +0500

    insert mark ledger

--- server/routes/marksledger.js
+++ server/routes/marksledger.js
@@ -4,10 +4,13 @@ var {
 	getMarks,
 	getMarksByFormID,
 	GetMarksByDept,
-	UpdateStatus, updateIsMarked
+	UpdateStatus,
+	updateIsMarked,
+	insertMarksLedger
 } = require('../controller/marksledger');
 
 routes.get('/', getMarks);
+routes.post('/', insertMarksLedger);
 routes.get('/:id', getMarksByFormID);
 routes.get('/dept/:id', GetMarksByDept);
 routes.put('/updatestatus/:id', UpdateStatus);
