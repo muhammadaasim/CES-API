a2ff6ac2aa4d59802e6e8678441f3f2552a8a13f
muhammadaasim <47273318+muhammadaasim@users.noreply.github.com>
2021-04-15 04:20:11 -0700

    changes

--- server/controller/teacherAssign.js
+++ server/controller/teacherAssign.js
@@ -197,7 +197,7 @@ module.exports.deassign = (req, res) => {
 	try {
 		connection.getConnection((err, connection) => {
 			if (err) throw err;
-			connection.query(`UPDATE f_subject_teacher_assign SET isdisabled = ? WHERE  id = ?`, [req.body.isdisabled,req.body.id ], function(
+			connection.query(`UPDATE subject SET isdisabled = ? WHERE  id = ?`, [req.body.isdisabled,req.body.id ], function(
 				err,
 				rows,
 				fields
