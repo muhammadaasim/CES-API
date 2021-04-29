d501d929c5b8ecc80ec747f64b10423d16fdcfd7
muhammadaasim <47273318+muhammadaasim@users.noreply.github.com>
2021-04-29 04:25:52 -0700

    changes

--- server/controller/recheck.js
+++ server/controller/recheck.js
@@ -35,7 +35,10 @@ module.exports.insertRecheck = (req, res) => {
 	try {
 		connection.getConnection((err, connection) => {
 			if (err) throw err;
-			connection.query(`INSERT INTO recheck SET ?`,[{std_id: req.body.std_id ,ml_id: req.body.ml_id,remarks: req.body.remarks,status: req.body.status }], function(err, rows, fields) {
+			connection.query(`INSERT INTO recheck SET ?`,[{std_id: req.body.std_id ,
+				ml_id: req.body.ml_id,remarks: req.body.remarks,status: req.body.status,
+				p_mid:req.body.p_mid,p_sessional:req.body.p_sessional,p_final:req.body.p_final,
+				p_total:req.body.p_total }], function(err, rows, fields) {
 				if (err) throw err;
 				console.log(rows);
 				res.send({ error: null, message: 'Insert successfully', result: rows });
@@ -46,6 +49,37 @@ module.exports.insertRecheck = (req, res) => {
 		res.send({ error: 'Error getting Recheck', department: [], success: 'Failed' });
 	}
 };
+
+
+module.exports.UpdateMarks = (req, res) => {
+	try {
+		connection.getConnection((err, connection) => {
+			if (err) throw err;
+			connection.query(
+				`UPDATE recheck SET n_sessional=?,n_mid=?,n_final=?,n_total=? WHERE id = ?`,
+				[ parseInt(req.body.sessional), parseInt(req.body.mid), parseInt(req.body.final), parseInt(	parseInt(req.body.mid) +
+					parseInt(req.body.sessionalmark) +
+					parseInt(req.body.finalmark)), parseInt(req.params.id) ],
+				function(err, rows, fields) {
+					if (err) throw err;
+					res.send({
+						error: null,
+						message: 'Marks Update successfully',
+						result: rows
+					});
+					connection.release((er) => console.log(er));
+				}
+			);
+		});
+	} catch (e) {
+		res.send({
+			error: 'Error getting updating data',
+			result: [],
+			success: 'Failed'
+		});
+	}
+};
+
 module.exports.UpdateStatus = (req, res) => {
 	try {
 		connection.getConnection((err, connection) => {
--- server/routes/recheck.js
+++ server/routes/recheck.js
@@ -1,9 +1,11 @@
 var routes = require('express').Router();
 
-var {getRecheck,getRecheckByDeptId,insertRecheck,UpdateStatus} = require('../controller/recheck')
+var {getRecheck,getRecheckByDeptId,insertRecheck,UpdateStatus,UpdateMarks} = require('../controller/recheck')
 routes.get('/', getRecheck)
 routes.post('/', insertRecheck)
 routes.get('/:id', getRecheckByDeptId)
 routes.put('/updatestatus/:id', UpdateStatus);
+routes.put('/updatemarks/:id', UpdateMarks);
+
 
 module.exports = routes;
